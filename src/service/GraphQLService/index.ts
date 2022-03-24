bacb8d6b05ec623e159aaf21a995decf1fb31a9b
Daniel Koch <koch@terrestris.de>
2022-03-24 11:40:21 +0100

    Init GraphQL service

---
+++ src/service/GraphQLService/index.ts
@@ -0,0 +1,52 @@
+import { getCsrfTokenHeader } from '../getCsrfTokenHeader';
+
+// TODO: Make this generic and more specific
+export interface GraphQLQueryObject {
+  query: string;
+  variables?: {
+    [key: string]: any;
+  };
+  operation?: null;
+};
+
+export interface GraphQLResponse<T> {
+  data: T;
+  errors?: any;
+};
+
+export interface GraphQLServiceOpts {
+  url: string;
+};
+
+export class GraphQLService {
+
+  private url: string;
+
+  constructor(opts: GraphQLServiceOpts = {
+    url: '/graphql'
+  }) {
+    this.url = opts.url;
+  }
+
+  async sendQuery<T>(query: GraphQLQueryObject, fetchOpts?: RequestInit): Promise<GraphQLResponse<T>> {
+    try {
+      const response = await fetch(this.url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          ...getCsrfTokenHeader()
+        },
+        body: JSON.stringify(query),
+        ...fetchOpts
+      });
+
+      const responseJson = await response.json() as GraphQLResponse<T>;
+
+      return responseJson;
+    } catch (error) {
+      throw new Error(`Error while requesting GraphQL: ${error}`);
+    }
+  }
+}
+
+export default GraphQLService;
