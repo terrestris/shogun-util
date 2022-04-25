3845b787f9fd3f6beaf90e94de293d56d4e19f01
Daniel Koch <koch@terrestris.de>
2022-04-25 08:36:33 +0200

    Fix default path

--- src/service/LayerService/index.ts
+++ src/service/LayerService/index.ts
@@ -4,7 +4,7 @@ import GenericService, { GenericServiceOpts } from '../GenericService';
 export class LayerService<T extends Layer> extends GenericService<T> {
 
   constructor(opts: GenericServiceOpts = {
-    basePath: '/users'
+    basePath: '/layers'
   }) {
     super(opts);
   }
