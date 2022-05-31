ce8ade99739c92fad5ece82f4b02d2ae0b357083
Daniel Koch <koch@terrestris.de>
2022-05-31 08:17:38 +0200

    fix: fix line length

--- src/service/UserService/index.ts
+++ src/service/UserService/index.ts
@@ -2,7 +2,8 @@ import GenericService, { GenericServiceOpts } from '../GenericService';
 
 import User, { KeycloakUserRepresentation, ProviderUserDetails } from '../../model/User';
 
-export class UserService<T extends User<S>, S extends ProviderUserDetails = KeycloakUserRepresentation> extends GenericService<T> {
+export class UserService<T extends User<S>,
+  S extends ProviderUserDetails = KeycloakUserRepresentation> extends GenericService<T> {
 
   constructor(opts: GenericServiceOpts = {
     basePath: '/users'
