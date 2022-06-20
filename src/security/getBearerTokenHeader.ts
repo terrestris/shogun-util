0930303457133bc2af168d302e49d6c3fe419c12
Daniel Koch <koch@terrestris.de>
2022-06-20 22:07:49 +0200

    feat: add getBearerTokenHeader util

---
+++ src/security/getBearerTokenHeader.ts
@@ -0,0 +1,17 @@
+import Keycloak from 'keycloak-js';
+
+export interface BearerTokenHeader {
+  'Authorization': string;
+}
+
+export const getBearerTokenHeader = (keycloak?: Keycloak): BearerTokenHeader | undefined => {
+  let accessToken = keycloak?.token;
+
+  if (!accessToken) {
+    return;
+  }
+
+  return {
+    'Authorization': `Bearer ${accessToken}`
+  };
+};
