937265179bfb0ccd482b090596bfa3b3e976cede
Daniel Koch <koch@terrestris.de>
2024-09-20 10:49:53 +0200

    chore: replace watch with chokidar in watchBuild script

--- watchBuild.js
+++ watchBuild.js
@@ -1,20 +1,20 @@
 #!/usr/bin/env node
-/* eslint-disable require-jsdoc */
 /* eslint-disable no-console */
 
 'use strict';
 
 const fs = require('fs-extra');
 const path = require('path');
-const watch = require('watch');
+const chokidar = require('chokidar');
 const util = require('util');
+const throttle = require('lodash/throttle');
 const exec = util.promisify(require('child_process').exec);
 
 const curDir = process.cwd();
 
 if (process.argv.length < 3) {
-  console.log('please specify target path');
-  console.log('for example ../shogun-gis-client/node_modules/@terrestris/shogun-util/');
+  console.log('Please specify the target path');
+  console.log('For example: ../shogun-gis-client/node_modules/@terrestris/shogun-util/');
   process.exit(0);
 }
 
@@ -24,24 +24,25 @@ const targetDistPath = path.join(curDir, process.argv[2], 'dist');
 const targetSrcPath = path.join(curDir, process.argv[2], 'src');
 
 if (!fs.existsSync(targetDistPath) ) {
-  throw new Error('target does not exist');
+  throw new Error('Target path does not exist');
 }
 
 async function buildAndCopy() {
-  console.log('run build');
+  console.log('Run build');
 
   try {
     const { stdout, stderr} = await exec('npm run build');
     console.log(stdout);
     console.log(stderr);
 
-    console.log(`copy dist & src from ${curDir} to ${path.join(curDir, process.argv[2])}`);
+    console.log(`Copy dist & src from ${curDir} to ${path.join(curDir, process.argv[2])}`);
+
     await fs.copy(distPath, targetDistPath);
     await fs.copy(sourcePath, targetSrcPath);
 
-    console.log('done');
+    console.log('Done');
   } catch (error) {
-    console.log('error');
+    console.log('Error');
     const { stdout, stderr } = error;
     console.log(stdout);
     console.log(stderr);
@@ -50,22 +51,9 @@ async function buildAndCopy() {
 
 buildAndCopy();
 
-let timeout;
+const throttled = throttle(buildAndCopy, 1000);
 
-function throttle(callback, time) {
-  if (!timeout) {
-    timeout = setTimeout(function () {
-      timeout = null;
-      callback();
-    }, time);
-  }
-}
-
-// eslint-disable-next-line no-unused-vars
-watch.watchTree(sourcePath, function (f, curr, prev) {
-  if (typeof f === 'object') {
-    console.log('watching');
-  } else {
-    throttle(buildAndCopy, 1000);
-  }
-});
+chokidar.watch(sourcePath)
+  .on('add', throttled)
+  .on('change', throttled)
+  .on('unlink', throttled);
