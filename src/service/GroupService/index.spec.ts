75b0b586356403a5394c0288c5117a146a1ccccc
Daniel Koch <koch@terrestris.de>
2024-11-11 11:34:08 +0100

    feat: add service methods to create users, groups and roles from the provider

--- src/service/GroupService/index.spec.ts
+++ src/service/GroupService/index.spec.ts
@@ -1,12 +1,24 @@
+import fetchSpy, {
+  failureResponse,
+  successResponse
+} from '../../spec/fetchSpy';
 import Group, { KeycloakGroupRepresentation } from '../../model/Group';
 import GenericEntityService from '../GenericEntityService';
 import GroupService from '.';
 
 describe('GroupService', () => {
+  let fetchMock: jest.SpyInstance;
   let service: GroupService<Group<KeycloakGroupRepresentation>, KeycloakGroupRepresentation>;
 
   beforeEach(() => {
     service = new GroupService<Group<KeycloakGroupRepresentation>, KeycloakGroupRepresentation>();
+  });
+
+  afterEach(() => {
+    if (fetchMock) {
+      fetchMock.mockReset();
+      fetchMock.mockRestore();
+    }
   });
 
   it('is defined', () => {
@@ -19,6 +31,23 @@ describe('GroupService', () => {
 
   it('has set the correct default path', () => {
     expect(service.getBasePath()).toEqual('/groups');
+  });
+
+  it('sends all required parameters to create all groups from the provider (createAllFromProvider)', async () => {
+    fetchMock = fetchSpy(successResponse([]));
+
+    await service.createAllFromProvider();
+
+    expect(fetchMock).toHaveBeenCalledWith('/groups/createAllFromProvider', {
+      headers: {},
+      method: 'POST'
+    });
+  });
+
+  it('throws an error if the groups from the provider couldn\'t be created (createAllFromProvider)', async () => {
+    fetchMock = fetchSpy(failureResponse());
+
+    await expect(service.createAllFromProvider()).rejects.toThrow();
   });
 
 });
--- src/service/RoleService/index.spec.ts
+++ src/service/RoleService/index.spec.ts
@@ -1,12 +1,24 @@
+import fetchSpy, {
+  failureResponse,
+  successResponse
+} from '../../spec/fetchSpy';
 import Role from '../../model/Role';
 import GenericEntityService from '../GenericEntityService';
 import RoleService from '.';
 
 describe('RoleService', () => {
+  let fetchMock: jest.SpyInstance;
   let service: RoleService<Role>;
 
   beforeEach(() => {
     service = new RoleService();
+  });
+
+  afterEach(() => {
+    if (fetchMock) {
+      fetchMock.mockReset();
+      fetchMock.mockRestore();
+    }
   });
 
   it('is defined', () => {
@@ -19,6 +31,24 @@ describe('RoleService', () => {
 
   it('has set the correct default path', () => {
     expect(service.getBasePath()).toEqual('/roles');
+  });
+
+
+  it('sends all required parameters to create all roles from the provider (createAllFromProvider)', async () => {
+    fetchMock = fetchSpy(successResponse([]));
+
+    await service.createAllFromProvider();
+
+    expect(fetchMock).toHaveBeenCalledWith('/roles/createAllFromProvider', {
+      headers: {},
+      method: 'POST'
+    });
+  });
+
+  it('throws an error if the roles from the provider couldn\'t be created (createAllFromProvider)', async () => {
+    fetchMock = fetchSpy(failureResponse());
+
+    await expect(service.createAllFromProvider()).rejects.toThrow();
   });
 
 });
