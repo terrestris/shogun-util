6f6d1829aeaa285a8bbd6bc50baceecb5e643f4c
Fritz Hoeing <fhoeing@terrestris.de>
2025-01-09 16:36:45 +0100

    feat: extends the util by a find by name function

--- src/service/ApplicationService/index.ts
+++ src/service/ApplicationService/index.ts
@@ -1,4 +1,5 @@
 import Application from '../../model/Application';
+import { getBearerTokenHeader } from '../../security/getBearerTokenHeader';
 import GenericEntityService, { GenericEntityServiceOpts } from '../GenericEntityService';
 
 export class ApplicationService<T extends Application> extends GenericEntityService<T> {
@@ -7,6 +8,26 @@ export class ApplicationService<T extends Application> extends GenericEntityServ
     basePath: '/applications'
   }) {
     super(opts);
+  }
+
+  async findOneByName(name: string, fetchOpts?: RequestInit): Promise<T> {
+    try {
+      const response = await fetch(`${this.basePath}/name/${name}`, {
+        method: 'GET',
+        headers: {
+          ...getBearerTokenHeader(this.keycloak)
+        },
+        ...fetchOpts
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error status: ${response.status}`);
+      }
+
+      return await response.json();
+    } catch (error) {
+      throw new Error(`Error while requesting a single entity: ${error}`);
+    }
   }
 
 }
